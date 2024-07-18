d48851169985b8a707d7b3782147418f1b5dc1b7
Bruno Perez <brunoperez24793@gmail.com>
2024-07-18 17:03:23 -0300

    .

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -11,42 +11,8 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      authorization: {
-        params: {
-          scope: 'openid email profile'
-        }
-      }
     }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  debug: true,
-  callbacks: {
-    async signIn({ account, profile }) {
-      console.log("Account:", account);
-      console.log("Profile:", profile);
-      if (account.provider === "google") {
-        return profile.email_verified && profile.email.endsWith("@gmail.com");
-      }
-      return true;
-    },
-    async redirect({ url, baseUrl }) {
-      console.log("Redirect URL:", url);
-      return baseUrl;
-    },
-    async session({ session, token }) {
-      console.log("Session:", session);
-      console.log("Token:", token);
-      return session;
-    },
-    async jwt({ token, account }) {
-      console.log("JWT Token:", token);
-      console.log("Account:", account);
-      if (account) {
-        token.accessToken = account.accessToken;
-      }
-      return token;
-    },
-  },
+  ]
 });
 
 export { handler as GET, handler as POST };
